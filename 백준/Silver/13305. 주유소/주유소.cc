5f70695fdf51b2ca8ac82b705ad2ce18a6268c53
yminjuu <124325672+yminjuu@users.noreply.github.com>
2024-02-13 21:27:13 +0900

    [Silver III] Title: 주유소, Time: 12 ms, Memory: 2676 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <queue>
+#include <string>
+using namespace std;
+int N;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    cin >> N;
+    int dist[N - 1];
+    int price[N];
+
+    for (int i = 0; i < N - 1; i++) cin >> dist[i];
+    for (int i = 0; i < N; i++) cin >> price[i];
+
+    int sum = 0;
+    int disSum = 0;
+    int currPrice = 1000000001;
+    for (int i = 0; i < N - 1; i++)
+    {
+        if (currPrice <= price[i])
+        {
+            disSum += dist[i];
+        }
+        else
+        {
+            sum += currPrice * disSum;
+            currPrice = price[i];
+            disSum = 0;
+            disSum += dist[i];
+        }
+    }
+    sum += currPrice * disSum;
+    cout << sum;
+
+    return 0;
+}
