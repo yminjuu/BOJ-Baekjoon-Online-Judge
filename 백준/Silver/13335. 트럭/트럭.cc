4ff88229b70fb91f47c6a26d0b07d7a9548930b9
Chee <124325672+yminjuu@users.noreply.github.com>
2024-07-25 09:38:10 +0900

    [Silver I] Title: 트럭, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/13335. 트럭/트럭.cc
+++ 백준/Silver/13335. 트럭/트럭.cc
@@ -1,14 +1,11 @@
 #include <iostream>
-#include <limits.h>
 #include <vector>
-#include <cstdlib>
 
 using namespace std; 
 
-int n, w, L, k;
-int sum=0, total =1;
-int front=0, back=0;
-vector<pair<int, int> > vec;
+int n, w, L;
+vector<int> truck_weights;
+
 
 int main()
 {
@@ -16,41 +13,36 @@ int main()
     cin.tie(0); cout.tie(0);
     
     cin >> n >> w >> L;
-    for (int i=0; i<n; i++){
-        cin >> k;
-        vec.push_back(make_pair(k, w));
+    truck_weights.resize(n);
+    for (int i = 0; i < n; i++) {
+        cin >> truck_weights[i];
     }
 
-    int chk =0;
-    for (int i=0; i<n; i++){
-        chk=0;
-        while (vec[i].first+sum>L && front<=back){ 
-            chk=1;
-            total++;
-            for (int j=front;j<=back; j++){
-                if (--vec[j].second<0){
-                    front = j+1;
-                    sum -= vec[j].first;
-                };
+    vector<pair<int, int>> bridge;
+    int sum = 0;
+    int time = 0;
+    int i = 0;
+
+    while (i < n || !bridge.empty()) {
+        time++;
+
+        if (!bridge.empty()) {
+            for (auto& truck : bridge) {
+                truck.second--;
+            }
+            if (bridge.front().second == 0) {
+                sum -= bridge.front().first;
+                bridge.erase(bridge.begin());
             }
         }
-        back=i;
-        sum+=vec[i].first;
-        if (chk==0) {
-            total++;
-            for (int j=front; j<=back; j++){
-                if (--vec[j].second<0){
-                    front= j+1;
-                    sum -= vec[j].first;
-                };
-            }
+
+        if (i < n && sum + truck_weights[i] <= L && bridge.size() < w) {
+            bridge.push_back({truck_weights[i], w});
+            sum += truck_weights[i];
+            i++;
         }
-        else vec[i].second--;
     }
 
-    if (vec[n-1].second>0) total+= vec[n-1].second;
-    
-    cout << total;
-
+    cout << time << '\n';
     return 0;
 }
