96d407ef3157fc705896a7350c4b843dc2a2dca6
y_mingd <124325672+yminjuu@users.noreply.github.com>
2025-01-07 13:50:29 +0900

    [Silver III] Title: 1로 만들기, Time: 44 ms, Memory: 52680 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+int N;
+int memo[1000001]= {0};
+
+int DP(int k){
+    if (k<=0) return 0;
+    if (k<=3) return memo[k];
+
+    int min=1000000;
+
+    if (!memo[k-1]) memo[k-1]= DP(k-1);
+    min = memo[k-1];
+
+    if (k%3==0) {
+        if (!memo[k/3]) memo[k/3]= DP(k/3); 
+        if (min > memo[k/3]) min = memo[k/3];
+    }
+
+    if (k%2==0) {
+        if (!memo[k/2]) memo[k/2]= DP(k/2);
+        if (min > memo[k/2]) min = memo[k/2];
+    }
+
+    memo[k]= min+1;
+    return min+1;
+}
+
+int main()
+{
+
+   ios_base::sync_with_stdio(false);
+   cin.tie(0); cout.tie(0);
+   memo[1]= 0; memo[2]=1; memo[3]=1;
+
+   cin >> N;
+
+   DP(N);
+
+   cout << memo[N];
+
+   return 0;
+}
