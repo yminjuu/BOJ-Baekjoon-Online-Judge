08c1d2e5f3d2ab4751855702e9f44245ab1220c8
y_mingd <124325672+yminjuu@users.noreply.github.com>
2025-03-28 14:58:57 +0900

    [level 2] Title: 더 맵게, Time: 214.54 ms, Memory: 39.2 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.cpp
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.cpp
@@ -9,22 +9,19 @@ int solution(vector<int> scoville, int K) {
     int answer = 0;
     
 //     최소 힙에 넣는다
-    priority_queue<int, vector<int>, greater<int>> pq;
-    for (int i=0; i< scoville.size(); i++){
-        pq.push(scoville.at(i));
-    }
+    priority_queue<int, vector<int>, greater<int>> pq (scoville.begin(), scoville.end());
     
-    while (!pq.empty()){
+    // 최종 목표는 top이 K 이상인 것
+    while (pq.top()<K){
         //     개별 음식의 스코빌 지수가 7 이상 (성공) or pq size가 2 미만 (실패) => 종료
-        if (pq.top()>=K) return answer;
         if (pq.size()<2) { return -1;}
         
          //     음식을 섞고
         int tmp= pq.top(); pq.pop();
-        tmp+= 2*pq.top(); pq.pop();
+        pq.push(tmp + 2*pq.top());
+        pq.pop();
         answer++;
-        
-        //     다시 heap에 추가
-        pq.push(tmp);      
     }
+    
+    return answer;
 }
