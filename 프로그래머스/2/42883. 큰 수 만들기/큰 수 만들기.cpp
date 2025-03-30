b1650098c888f7cb7b229a9655f5880bc79da3f4
y_mingd <124325672+yminjuu@users.noreply.github.com>
2025-03-30 12:17:50 +0900

    [level 2] Title: 큰 수 만들기, Time: 5777.26 ms, Memory: 5.55 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
@@ -7,20 +7,26 @@ using namespace std;
 
 string solution(string number, int k) {
     string answer= "";
-    // 큰 숫자를 만들어가는 과정
-    // 자릿수가 클수록 더 큰 숫자가 들어가야 좋음.
     
-    for (int i=0; i<number.size(); i++){
-        if (number[i]<number[i+1]){
-            number.erase(number.begin()+i); // number이라는 string에서 i번째 인덱스 삭제 후 리사이징
-            k--;
-            i=-1; // i 0부터 다시 시작하도록
+    int biggest = 0;
+    int idx = 0;
+    // k에 하나씩 더해서 k와 number size이 같아질 때까지
+    for (int i=0; i<number.size(); i++) {
+        if (number[i] > biggest) {
+            biggest = number[i];
+            idx= i; // 가장 큰 수의 인덱스를 기억한다.
         }
         
-        if (k<=0) break;
+        
+        // 최댓값을 갱신해주다가 k(멈춰서서 해당 최댓값을 answer에 넣어줘야 하는 위치)
+        if (i==k) {
+            answer.push_back(number[idx]);
+            k++;
+            i= idx; // 현재 인덱스 다음 위치부터 찾도록
+            biggest =0; idx=0;
+        }
+        
     }
-    
-    answer= number.substr(0, number.size()-k);
     
     return answer;
 }
