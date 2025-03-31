fb4c8db449e3949b6eb0d0315c6da8e904883313
y_mingd <124325672+yminjuu@users.noreply.github.com>
2025-03-31 17:47:48 +0900

    [level 2] Title: 조이스틱, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.cpp
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.cpp
@@ -6,11 +6,10 @@
 using namespace std;
 
 int solution(string name){
-    int upDownMove[26] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,12,11,10,9,8,7,6,5,4,3,2,1};
     int ans = 0, n = name.size();
     int leftRightMove = n-1;
     for(int x = 0; x < n; x++){
-        ans += upDownMove[name[x]-'A'];
+        ans += min(name[x]-'A', abs(name[x]-'Z')+1);
         int y = x + 1; 
         while( y < n && name[y] == 'A') y++;
         leftRightMove = min( leftRightMove, min( x+x+(n-y), x+(n-y)+(n-y) ) );
