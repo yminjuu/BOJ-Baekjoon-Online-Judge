957b5fe5c9b7811ee7c0bf6fd94986e0ca0fb4bf
y_mingd <124325672+yminjuu@users.noreply.github.com>
2025-07-21 19:10:14 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 112 ms, Memory: 10108 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -1,34 +1,33 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
-using namespace std;
 
-int N, M, x, y, xx, yy;
-int arr[1025][1025];
-int pSum[1025][1025] = {
-    0,
-};
+using namespace std;
+int N,M; // 표의 크기, 합을 구해야 하는 횟수
+int x1,y1,x2,y2;
+int dp[1025][1025]= {0,};
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
+   ios_base::sync_with_stdio(false);
+   cin.tie(0); cout.tie(0);
 
     cin >> N >> M;
+    int table[N+1][N+1];
 
-    for (int i = 1; i <= N; i++)
-    {
-        for (int j = 1; j <= N; j++)
-        {
-            cin >> arr[i][j];
-            pSum[i][j] = arr[i][j] + pSum[i - 1][j] + pSum[i][j - 1] - pSum[i - 1][j - 1];
+    for (int i=1; i<=N; i++){
+        for (int j=1; j<=N; j++){
+            cin >> table[i][j];
+            dp[i][j]= table[i][j]+dp[i-1][j]+dp[i][j-1]-dp[i-1][j-1];
+             // 내 위 원소 dp 값 + 왼쪽 원소 dp 값 - 왼쪽위원소 dp 값 (중복 범위)
         }
     }
 
-    for (int i = 0; i < M; i++)
-    {
-        cin >> x >> y >> xx >> yy;
-        cout << (pSum[xx][yy] - pSum[x - 1][yy] - pSum[xx][y - 1] + pSum[x - 1][y - 1]) << "\n";
+    for (int i=0; i<M; i++){
+        cin >> x1 >> y1 >> x2 >> y2;
+        cout << dp[x2][y2]-dp[x1-1][y2]-dp[x2][y1-1]+dp[x1-1][y1-1] << "\n";
+        // 합을 구해 출력
     }
 
-    return 0;
+   return 0;
 }
