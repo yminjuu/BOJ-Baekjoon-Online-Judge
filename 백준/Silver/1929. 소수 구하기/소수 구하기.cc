4a9db0daf843497758fb6b7d6d389c0548522cda
y_mingd <124325672+yminjuu@users.noreply.github.com>
2026-01-30 13:47:42 +0900

    [Silver III] Title: 소수 구하기, Time: 404 ms, Memory: 2272 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+   ios_base::sync_with_stdio(false);
+   cin.tie(0); cout.tie(0);
+   
+   int N, M; 
+   cin >> M >> N;
+
+    if (M==1) M++;
+    vector<bool> vec(N+1, false);
+    
+    for (int i=2; i<=sqrt(N); i++){
+        if (!vec[i]){
+            for (int j=i+1; j<=N; j++){
+                if (j%i==0 && !vec[j]) vec[j]= true;
+            }   
+        }
+    }
+
+    for (int i=M; i<=N; i++){
+        if (!vec[i]) cout << i << "\n";
+    }
+
+   return 0;
+}
