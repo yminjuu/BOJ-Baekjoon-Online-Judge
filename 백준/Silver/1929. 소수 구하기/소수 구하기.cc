6f749a33753ba9603dcab4f5fc5401221d168515
y_mingd <124325672+yminjuu@users.noreply.github.com>
2026-01-30 13:50:27 +0900

    [Silver III] Title: 소수 구하기, Time: 288 ms, Memory: 2272 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.cc
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -20,7 +20,7 @@ int main()
     
     for (int i=2; i<=sqrt(N); i++){
         if (!vec[i]){
-            for (int j=i+1; j<=N; j++){
+            for (int j=i*i; j<=N; j++){
                 if (j%i==0 && !vec[j]) vec[j]= true;
             }   
         }
