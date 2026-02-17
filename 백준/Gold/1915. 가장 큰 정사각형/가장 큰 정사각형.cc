9eb1349bc6f88cc0f6bca122b666544a5deb258b
y_mingd <124325672+yminjuu@users.noreply.github.com>
2026-02-17 16:41:49 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 24 ms, Memory: 5972 KB -BaekjoonHub

--- 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.cc
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.cc
@@ -7,7 +7,6 @@
 
 using namespace std;
 int arr[1004][1004];
-int dp[1004][1004];
 
 int main()
 {
@@ -28,20 +27,20 @@ int main()
     long mx= 0;
     for (int i=1; i<=N; i++){
         for (int j=1; j<=M; j++){
-            if ((arr[i][j-1]==1 && arr[i-1][j]==1) && (arr[i-1][j-1]==1 && arr[i][j]==1)){
-                int tmp= min(min(dp[i-1][j-1], dp[i-1][j]), dp[i][j-1]);
-                if ((dp[i][j-1]>=tmp && dp[i-1][j]>=tmp) && (dp[i-1][j-1]>=tmp && tmp!=0)){
+            if ((arr[i][j-1]>=1 && arr[i-1][j]>=1) && (arr[i-1][j-1]>=1 && arr[i][j]>=1)){
+                int tmp= min(min(arr[i-1][j-1], arr[i-1][j]), arr[i][j-1]);
+                if ((arr[i][j-1]>=tmp && arr[i-1][j]>=tmp) && (arr[i-1][j-1]>=tmp && tmp!=0)){
                     // 값이 모두 같다면 -> 새로운 크기의 정사각형으로 확장
-                    dp[i][j]= pow((sqrt(tmp)+1), 2);
-                    if (mx<dp[i][j]) mx= dp[i][j];
+                    arr[i][j]= pow((sqrt(tmp)+1), 2);
+                    if (mx<arr[i][j]) mx= arr[i][j];
                 } else {
                     // 값이 다르다면 확장은 불가능
-                    dp[i][j]= 4;
+                    arr[i][j]= 4;
                 }
             } else {
-                if (arr[i][j]) dp[i][j]= 1;
+                if (arr[i][j]) arr[i][j]= 1;
             }
-            if (mx<dp[i][j]) mx= dp[i][j];
+            if (mx<arr[i][j]) mx= arr[i][j];
         }
     }
 
