3817db688e135b1882e19068c62009dcd4575832
y_mingd <124325672+yminjuu@users.noreply.github.com>
2026-02-22 02:02:57 +0900

    [level 4] Title: 년, 월, 성별 별 상품 구매 회원 수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131532. 년， 월， 성별 별 상품 구매 회원 수 구하기/년， 월， 성별 별 상품 구매 회원 수 구하기.sql
+++ 프로그래머스/4/131532. 년， 월， 성별 별 상품 구매 회원 수 구하기/년， 월， 성별 별 상품 구매 회원 수 구하기.sql
@@ -1,9 +1,5 @@
 -- 코드를 입력하세요
-with no_null as (
-    select user_id, gender
-    from user_info
-    where gender is not null
-)
 select year(sales_date), month(sales_date), gender, count(distinct(user_id))
-from no_null natural join online_sale
+from user_info natural join online_sale
+where gender is not null
 group by year(sales_date), month(sales_date), gender;
