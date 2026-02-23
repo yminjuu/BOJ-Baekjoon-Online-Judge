8b8a8e132c9cf1559565919e71d260efd6b4e40d
y_mingd <124325672+yminjuu@users.noreply.github.com>
2026-02-24 00:37:11 +0900

    [Gold III] Title: 파일 합치기, Time: 116 ms, Memory: 2884 KB -BaekjoonHub

--- 백준/Gold/11066. 파일 합치기/파일 합치기.cc
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.cc
@@ -13,25 +13,26 @@ void file(){
     cin >> K;
     int dp[K+1][K+1];
     int cost[K+1];
+    int sum[K+1]; // 구간합을 미리 구해둠
     fill(&dp[0][0], &dp[K][K], INT_MAX);
-    
-    for (int i=0; i<K; i++){
+
+    sum[0]=0;
+    for (int i=1; i<=K; i++){
         cin >> cost[i];
+        sum[i]= sum[i-1]+cost[i];
         dp[i][i]= 0; // 합치는 비용 0
     }
 
     for (int length= 1; length < K; length++){
-        for (int start= 0; start+length< K; start++){
+        for (int start= 1; start+length<= K; start++){
             for (int mid= start; mid<start+length; mid++){
                 dp[start][start+length] = (min(dp[start][start+length], abs(dp[start][mid]+dp[mid+1][start+length])));
             }
-            for (int i=start; i<=start+length; i++){
-                dp[start][start+length]+= cost[i];
-            }
+            dp[start][start+length]+= (sum[start+length]- sum[start-1]);
         }
     }
 
-    cout << dp[0][K-1] << "\n";
+    cout << dp[1][K] << "\n";
 }
 
 int main()
