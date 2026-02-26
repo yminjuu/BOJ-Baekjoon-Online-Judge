2c0f53cc48f392091b1583c6f5284de31bdf309d
y_mingd <124325672+yminjuu@users.noreply.github.com>
2026-02-26 17:19:00 +0900

    [level 2] Title: 구명보트, Time: 1.84 ms, Memory: 5.11 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/README.md
+++ 프로그래머스/2/42885. 구명보트/README.md
@@ -1,10 +1,10 @@
 # [level 2] 구명보트 - 42885 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885) 
 
 ### 성능 요약
 
-메모리: 5.07 MB, 시간: 2.25 ms
+메모리: 5.11 MB, 시간: 1.84 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 01일 20:24:29
+2026년 02월 26일 17:18:58
 
 ### 문제 설명
 
--- 프로그래머스/2/42885. 구명보트/구명보트.cpp
+++ 프로그래머스/2/42885. 구명보트/구명보트.cpp
@@ -5,20 +5,28 @@
 
 using namespace std;
 
+// 최대한 적은 개수의 보트를 사용해서 사람들을 태워야 한다
+// 최대한 limit에 꽉 차게
 int solution(vector<int> people, int limit) {
-    int answer = 0; // 배의 개수
-    sort(people.begin(), people.end(), greater<>()); // 내림차순 정렬
-    deque<int> deq; // 앞뒤가 다 뚫려있는
+    int answer = 0; 
     
-    for (int i=0; i< people.size(); i++) deq.push_back(people[i]);
-    
-    while (!deq.empty()){
-        if (deq.size()==1) {deq.pop_front(); answer++; break;}
-        if (deq.front()+deq.back()<=limit) {
-            deq.pop_front(); deq.pop_back();
-        } else {
+    sort(people.begin(), people.end()); 
+    // 정렬 내림차순은?
+    deque<long> deq(people.begin(), people.end());
+        
+    while (deq.size()>=2){
+        int last = deq.back();
+        deq.pop_back();
+        
+        if (deq.size() >=1 && last + deq.front()<=limit) {
+            last += deq.front();
             deq.pop_front();
         }
+        
+        answer++;
+    }
+    
+    if (deq.size()==1) {
         answer++;
     }
     
