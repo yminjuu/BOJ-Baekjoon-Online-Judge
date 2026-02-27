237e74b3944f6544c79e58ac4b1628dd21533a35
y_mingd <124325672+yminjuu@users.noreply.github.com>
2026-02-27 21:17:39 +0900

    [level 3] Title: 단어 변환, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
@@ -1,40 +1,54 @@
 #include <string>
 #include <vector>
 #include <queue>
+#include <climits>
 
 using namespace std;
 
-bool compStr(string A, string B){
-    int sum=0;
-    for (int i=0; i<A.length(); i++){
-        if (A[i]!=B[i]) sum++;
-    }
-    if (sum==1) return true;
-    else return false;
-}
+string goal;
+int answer = INT_MAX;
+int sz;
+vector<string> gWords;
+int visited[52];
 
-// 전체: BFS 사용
-// 문제: 특정 문자열 간 얼마나 차이나는지를 어떻게 하면 효율적으로 판단할까?
-int solution(string begin, string target, vector<string> words) {
-    int lgth= words.size();
-    vector<bool> visited(lgth, false);
-    queue<pair<string,int>> q;
-    q.push({begin,0});
+void dfs(string start, int cnt) {
+    if (start==goal) {
+        answer = min(cnt, answer);
+        return;
+    }
     
-    while (!q.empty()){
-        string curr = q.front().first;
-        int cnt= q.front().second;
-        q.pop();
-        
-        if (curr==target) return cnt;
-        
-        for (int i=0; i<lgth; i++){
-            if (!visited[i] && compStr(curr, words[i])){
-                visited[i]= true;
-                q.push({words[i], ++cnt});
-            }
+    // 1글자만 다른 거 찾아서 dfs 진행
+    for (int i=0; i< gWords.size(); i++){
+        int tmp=0;
+        if (visited[i]) continue;
+        for (int j=0; j< sz; j++){
+            if (start[j]!= gWords[i][j]) tmp++;
+        }
+        if (tmp==1) {
+            visited[i]= 1;
+            dfs(gWords[i], cnt+1);
+            visited[i]=0;
         }
     }
-            
-    return 0;
+}
+
+int solution(string begin, string target, vector<string> words) {
+    gWords= words;
+    sz= begin.length();
+    goal= target;
+    
+    for (int i=0; i<words.size(); i++){
+        int cnt=0;
+        for (int j=0; j<sz; j++){
+            if (begin[j]!=words[i][j]) cnt++;
+        }
+        if (cnt==1) {
+            visited[i]= 1;
+            dfs(words[i], 1);
+            visited[i]=0;
+        }
+    }
+  
+    if (answer==INT_MAX) return 0;
+    else return answer;
 }
