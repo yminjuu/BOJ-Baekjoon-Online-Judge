bccab955ee60ebb72a00cabc83b7b8e1ef79c3fd
y_mingd <124325672+yminjuu@users.noreply.github.com>
2026-02-28 11:15:36 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -1,18 +1,13 @@
--- 코드를 작성해주세요
-select 
--- grade를 정해서 having 사용하기
-case 
-    -- 모든 터플에 대해 front end 스킬 존재 여부 확인
-    when sum(case when s.category='Front end' then 1 else 0 end) > 0 and d.skill_code & (select code from skillcodes where name='Python') > 0 then 'A'
-    when d.skill_code & (select code from skillcodes where name='C#') then 'B'
-    when sum(case when s.category='Front end' then 1 else 0 end) > 0 then 'C'
-    else 'D'
-end as grade
-, d.id, d.email
-from developers d, skillcodes s
-where 
--- 내가 가진 skillcode만 남도록
-d.skill_code & s.code > 0
-group by d.id, d.skill_code, d.email
-having grade < 'D'
-order by 1, 2;
+
+
+    select
+    case
+        when (skill_code & (select code from skillcodes where name='Python') > 0) and exists (select 1 from skillcodes where code & skill_code > 0 and category='Front end') then 'A'
+        when (skill_code & (select code from skillcodes where name='C#')>0) then 'B'
+        when exists (select 1 from skillcodes where code & skill_code > 0 and category='Front end') then 'C'
+        else null
+    end as grade, id, email
+    from developers
+    group by id, email, grade
+    having grade is not null
+    order by grade, id;
