d985d143770037fd7c4770e3e403316853950cfa
y_mingd <124325672+yminjuu@users.noreply.github.com>
2026-03-01 22:11:51 +0900

    [Silver III] Title: 소수 구하기, Time: 12 ms, Memory: 5744 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.cc
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -15,19 +15,20 @@ int main()
    int N, M; 
    cin >> M >> N;
 
-    if (M==1) M++;
-    vector<bool> vec(N+1, false);
-    
-    for (int i=2; i<=sqrt(N); i++){
-        if (!vec[i]){
-            for (int j=i*i; j<=N; j++){
-                if (j%i==0 && !vec[j]) vec[j]= true;
-            }   
+    int visited[N+1];
+    fill(&visited[0], &visited[N]+1, 0);
+    visited[1] = 1; // 예외처리
+
+    // N 제곱근까지만
+    for (int i=2; i<= sqrt(N); i++){
+        if (visited[i]) continue; // 이미 소수가 아님 => 지워졌을 것
+        for (int j=i+i ; j<=N; j+=i) {
+            if (!visited[j]) visited[j]= 1;
         }
     }
 
     for (int i=M; i<=N; i++){
-        if (!vec[i]) cout << i << "\n";
+        if (!visited[i]) cout << i << "\n";
     }
 
    return 0;
