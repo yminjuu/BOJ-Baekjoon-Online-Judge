ecd226bdeb7ce9c89845b310f138158334bd6a11
y_mingd <124325672+yminjuu@users.noreply.github.com>
2026-03-03 18:00:53 +0900

    [Gold IV] Title: 연구소, Time: 40 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.cc
+++ 백준/Gold/14502. 연구소/연구소.cc
@@ -2,85 +2,86 @@
 #include <vector>
 #include <algorithm>
 #include <queue>
+#include <string>
 #include <cstring>
 
+#define x first
+#define y second
+
 using namespace std;
-const int maxNM=8;
-int N,M;
-int arr[maxNM][maxNM]; // 정보 저장
-int tmpArr[maxNM][maxNM]; // 카운팅을 위한 임시 배열
-bool visited[maxNM*maxNM]; // 방문 여부
+using p = pair<int,int>;
 
-vector<pair<int,int> >  pairVec; // 쌍을 저장
-int near[2][4] = {{0,0,1,-1}, {1,-1,0,0}}; // 인접 배열
+int N, M; // 매우 작다 최대 (3,8)
+int arr[10][10];
+int tmp[10][10];
+int safeAns=0; // 안전 영역 크기 저장
 
-int ans=0;
+int dx[4]= {1,-1,0,0};
+int dy[4]= {0,0,1,-1};
 
-void bfs(){
-    queue<pair<int, int> > q; // 바이러스를 저장하는 큐
+int visited[10][10];
+vector<p> candidates; // 모든 벽 세우기 가능한 바이러스 인접벽들의 좌표를 저장
+vector<p> selected;
 
+void print(){
     for (int i=0; i<N; i++){
-        for (int j=0; j<M; j++){
-            if (tmpArr[i][j]==2){
-                // 바이러스가 있다면 큐에 넣음
-                q.push({i,j});
-            }
+        for (int j=0; j<M ;j++){
+            cout << tmp[i][j] << " ";
         }
+        cout <<"\n";
     }
-
-    // bfs를 사용하여 바이러스의 근원지부터 시작해서 퍼질 수 있는 곳까지 반복
-    while (q.size()){
-        int x= q.front().first;
-        int y= q.front().second;
-        q.pop();
-
-        for (int k=0; k<4; k++){
-            int nx= x+ near[0][k];
-            int ny = y+near[1][k];
-
-            if (nx>=0 && nx<N && ny>=0 && ny<M && tmpArr[nx][ny]==0){
-                tmpArr[nx][ny]=2; // 바이러스가 퍼짐
-                q.push({nx,ny});
-            }
-        }
-    }
-
-    int cnt =0;
-    for (int i=0; i<N; i++){
-        for (int j=0; j<M; j++){
-            if (tmpArr[i][j]==0) {
-                cnt++; // 안전구역을 카운팅
-            }
-        }
-    }
-    ans = max(ans,cnt);
 }
 
-void makeWall(int cur, int idx){
-    // 3가지 벽 세울 곳을 다 골랐다면 (브루트포스)
-    if (cur==3){
-        memcpy(tmpArr, arr, sizeof(arr)); // 원본 배열로부터 임시로 배열을 copy해서 만듦
+void spreadVirus(int x, int y){
+    for (int d=0; d<4; d++){
+        int newX= x+dx[d]; int newY= y+dy[d];
+        if (newX>=0 && newX<N && newY>=0 && newY<M && tmp[newX][newY]==0){
+            visited[newX][newY]= 1;
+            tmp[newX][newY]=2;
+            spreadVirus(newX, newY);
+        }
+    }
+}
 
-        int cnt= 0;
-        for (int i=0; i<pairVec.size(); i++){
-            if (cnt==3) break;
-            if (visited[i]) {
-                int x= pairVec[i].first;
-                int y= pairVec[i].second;
-                tmpArr[x][y]= 1; // 임시 배열에 벽을 세웠다고 가정
-                cnt++;
+int spreadVirusAndCnt(){
+    memcpy(tmp, arr, sizeof(arr)); // 2번째 인자가 원본
+
+    for (int i=0; i<3; i++){
+        int wallX= selected[i].x; int wallY = selected[i].y;
+        tmp[wallX][wallY]= 1;
+    }
+
+    fill(&visited[0][0], &visited[9][9]+1, 0);
+
+    for (int i=0; i<N; i++){
+        for (int j=0; j<M; j++){
+            if (tmp[i][j]==2 && !visited[i][j]){
+                visited[i][j]=1;
+                spreadVirus(i,j);
             }
         }
-        bfs(); // 안전 영역을 구하기 위해 bfs 사용
+    }
+
+    int ret =0;
+    for (int i=0; i<N; i++){
+        for (int j=0; j<M; j++){
+            if (tmp[i][j]==0) ret++;
+        }
+    }
+    return ret;
+}
+
+void dfs(int startIdx, int cnt){
+    if (cnt==3){
+        int res = spreadVirusAndCnt();
+        safeAns= max(res, safeAns);
         return;
     }
 
-
-    for (int i=idx; i<pairVec.size(); i++){
-        if (visited[i]) continue;
-        visited[i]=true;
-        makeWall(cur+1, i);
-        visited[i]=false;
+    for (int i=startIdx+1; i<candidates.size(); i++){
+        selected.push_back({candidates[i].x, candidates[i].y});
+        dfs(i, cnt+1);
+        selected.pop_back();
     }
 }
 
@@ -88,19 +89,23 @@ int main()
 {
    ios_base::sync_with_stdio(false);
    cin.tie(0); cout.tie(0);
+   
+   cin >> N >> M;
 
-    cin >> N >> M;
     for (int i=0; i<N; i++){
         for (int j=0; j<M; j++){
             cin >> arr[i][j];
-            if (arr[i][j]==0)
-                pairVec.push_back({i,j}); // 벽을 세울 수 있는 곳을 저장해둠
-        }   
+            if (arr[i][j]==0) candidates.push_back({i,j});
+        }
     }
 
-    makeWall(0,0);
-   
-    cout << ans;
+    for (int i=0; i<candidates.size(); i++){
+        selected.push_back({candidates[i].x, candidates[i].y});
+        dfs(i, 1);
+        selected.pop_back();
+    }
+
+    cout << safeAns;
 
    return 0;
 }
