82cb99aaf330d722c99472947f5510c38a296285
y_mingd <124325672+yminjuu@users.noreply.github.com>
2026-03-06 16:14:38 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -1,13 +1,11 @@
-
-
-    select
+select grade, id ,email
+from (select (
     case
-        when (skill_code & (select code from skillcodes where name='Python') > 0) and exists (select 1 from skillcodes where code & skill_code > 0 and category='Front end') then 'A'
-        when (skill_code & (select code from skillcodes where name='C#')>0) then 'B'
-        when exists (select 1 from skillcodes where code & skill_code > 0 and category='Front end') then 'C'
-        else null
-    end as grade, id, email
-    from developers
-    group by id, email, grade
-    having grade is not null
-    order by grade, id;
+        when skill_code & (select code from skillcodes where name='Python') > 0 and exists (select 1 from skillcodes where category='Front end' and skill_code & code > 0) then 'A'
+        when skill_code & (select code from skillcodes where name='C#') > 0 then 'B'
+        when exists (select 1 from skillcodes where category='Front end' and skill_code & code > 0) then 'C'
+    end
+) as grade, id, email
+from developers) as a
+where grade is not null
+order by grade, id;
