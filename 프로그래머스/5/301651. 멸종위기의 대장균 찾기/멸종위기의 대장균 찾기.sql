fcd49db51318a71b4e8123ea335ab7b1d7c6b199
y_mingd <124325672+yminjuu@users.noreply.github.com>
2026-03-06 15:49:41 +0900

    [level 5] Title: 멸종위기의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
+++ 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
@@ -1,24 +1,25 @@
 with recursive g as (
-    -- anchor
     select id, parent_id, 1 as generation
     from ecoli_data
     where parent_id is null
     
     union all
     
-    -- recursive
-    select e.id, e.parent_id, g.generation+1 as generation
-    from ecoli_data e, g
-    where g.id= e.parent_id
-)
+    select a.id, a.parent_id, generation+1 as generation
+    from ecoli_data a, g b
+    where b.id=a.parent_id
+),
 
--- 만약 여기서 0인 그룹도 포함되게 하려면??
-select count(id) as count, generation
-from g
-where id not in (
-    select parent_id
+parents as (
+    select distinct(parent_id) as parents_ids
     from ecoli_data
     where parent_id is not null
 )
-group by generation
-order by generation;
+
+select sum(
+    case when id in (select parents_ids from parents) then 0
+    else 1
+    end
+) as count, generation
+from g
+group by generation;
