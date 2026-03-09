9551822d61908b9bc92c5cd2b39dc19093329fa2
y_mingd <124325672+yminjuu@users.noreply.github.com>
2026-03-09 14:44:40 +0900

    [Gold III] Title: 파티, Time: 0 ms, Memory: 2336 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.cc
+++ 백준/Gold/1238. 파티/파티.cc
@@ -15,36 +15,35 @@ struct Road {
 };
 
 int N,M,X;
-vector<Road> path[1002];
+vector<Road> normal_adj[1002], reverse_adj[1002];
 
-int dijkstra(int s, int e){
-    int minPath[N+1];
-    fill(&minPath[0], &minPath[N]+1, MAX);
+// dist: 배열 포인터
+// adj : 포인터
+void dijkstra(int s, int dist[], vector<Road> adj[]){
+    fill(dist, dist+N+1, MAX);
     priority_queue<Road> pq;
 
+    dist[s]=0;
     pq.push({s, 0});
+    
     while (!pq.empty()){
         int e= pq.top().e; int time = pq.top().t;
         pq.pop();
 
-        // 제일 먼저 나온 최단경로일 때만
-        if (minPath[e] > time){
-            minPath[e]= time;
+        if (dist[e] < time) continue;
 
-            // 방문하게 된 노드에서 연결된 노드
-            for (int i=0; i<path[e].size(); i++){
-                int next = path[e][i].e;
-                int nextT =path[e][i].t;
+        // 방문하게 된 노드에서 연결된 노드
+        for (auto& edge: adj[e]){
+            int next = edge.e;
+            int nextT =edge.t;
 
-                // 유의미한 경로일 때에만
-                if (minPath[next] > minPath[e]+nextT){
-                    pq.push({next, minPath[e]+nextT});
-                }
+            // 유의미한 경로일 때에만 pq에 넣는다.
+            if (dist[next] > dist[e]+nextT){
+                dist[next]= dist[e]+nextT;
+                pq.push({next, dist[e]+nextT});
             }
         }
     }
-
-    return minPath[e];
 }
 
 int main()
@@ -58,18 +57,24 @@ int main()
     while (M--){
         int s, e, t;
         cin >> s >> e >> t;
-        path[s].push_back({e,t});
+        normal_adj[s].push_back({e,t});
+        reverse_adj[e].push_back({s,t});
     }
 
-    // 모든 노드에서 다익스트라로 최단 왕복 경로 구해서 max 값과 비교
+    int costs[N+1];
+    int dist[N+1];
+
+    // 정방향 (X-> i) 구함
+    dijkstra(X, dist, normal_adj);
+    for (int i=1; i<=N; i++) costs[i]= dist[i];
+    
+    // 역방향을 통해 모든 (i->X) 구함
+    dijkstra(X, dist, reverse_adj);
+
     int mx=0;
-    for (int i=1; i<=N; i++){
-        int cost=0;
-
-        cost+= dijkstra(i, X);
-        cost+= dijkstra(X,i);
-
-        mx = max(mx, cost);
+    for (int i=1; i<=N; i++) {
+        costs[i]+= dist[i];
+        mx= max(costs[i], mx);
     }
 
     cout << mx;
