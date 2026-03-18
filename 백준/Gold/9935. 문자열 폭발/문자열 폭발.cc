666087f67b59e49f89d893b1e20243b33a54834a
y_mingd <124325672+yminjuu@users.noreply.github.com>
2026-03-18 14:08:01 +0900

    [Gold IV] Title: 문자열 폭발, Time: 12 ms, Memory: 6056 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
@@ -3,48 +3,42 @@
 #include <algorithm>
 #include <queue>
 #include <string>
-#include <stack>
 
 using namespace std;
-using p= pair<char,int>;
 
 int main()
 {
    ios_base::sync_with_stdio(false);
    cin.tie(0); cout.tie(0);
    
-   string str, explode;
+   string ans="";
+    string str, explode;
     cin >> str >> explode;
+    int len = explode.length();
 
-    stack<p> stck;
-
-    int idx=0;
     for (int i=0; i<str.length(); i++){
-        if (explode.at(idx)==str.at(i)){
-            idx++;
-        } else if (explode.at(0)==str.at(i)){
-            idx=1;
-        } else idx=0;
-        stck.push({str.at(i), idx});
+        ans.push_back(str.at(i));
 
-        if (idx==explode.length()){
-            idx=0;
-            for (int i=0; i<explode.length(); i++) stck.pop();
-
-            if (!stck.empty())
-                idx = stck.top().second;
+        if (str.at(i)==explode.back() && ans.length()>= len){
+            // 끝이 같다면 검사 시작
+            bool isSame= true;
+            for (int j=0; j<len; j++){
+                if (ans[ans.length()-1-j]!= explode[len-1-j]) {
+                    isSame= false;
+                    break;
+                }
+            }
+            
+            if (isSame){
+                for (int j=0; j<len; j++){
+                    ans.pop_back();
+                }
+            }
         }
     }
 
-    string ans=""; // string을 vector처럼 사용
-    while (!stck.empty()){
-        ans.push_back(stck.top().first);
-        stck.pop();
-    }
-
-    reverse(ans.begin(), ans.end());
-    if (ans.length()==0) ans="FRULA";
-    cout << ans;
+    if (ans.length()==0) cout << "FRULA";
+    else cout << ans;
 
    return 0;
 }
