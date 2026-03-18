a5cc41e2f16534c3592a0aa9969adc0804a61975
y_mingd <124325672+yminjuu@users.noreply.github.com>
2026-03-18 13:42:23 +0900

    [Gold IV] Title: 문자열 폭발, Time: 16 ms, Memory: 11484 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
@@ -1,59 +1,50 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <deque>
+#include <queue>
 #include <string>
+#include <stack>
 
 using namespace std;
+using p= pair<char,int>;
 
 int main()
 {
    ios_base::sync_with_stdio(false);
    cin.tie(0); cout.tie(0);
    
-    string str;
-    cin >> str;
+   string str, explode;
+    cin >> str >> explode;
 
-    string explode;
-    cin >> explode;
+    stack<p> stck;
 
-    deque<int> vec; // idx를 저장
+    int idx=0;
+    for (int i=0; i<str.length(); i++){
+        if (explode.at(idx)==str.at(i)){
+            idx++;
+        } else if (explode.at(0)==str.at(i)){
+            idx=1;
+        } else idx=0;
+        stck.push({str.at(i), idx});
 
-    string arr[str.length()+1]; // 나까지 포함되는 curr 문자열을 저장한다.
+        if (idx==explode.length()){
+            idx=0;
+            for (int i=0; i<explode.length(); i++) stck.pop();
 
-    int N= str.length();
-    string curr="";
-    for (int i=0; i<N; i++){
-        if (str.at(i)== explode.at(curr.length())){
-            curr+= str.at(i);
-        } else if (str.at(i)==explode.at(0)) {
-            curr= str.at(i);
-        } else {
-            // 초기화
-            curr= "";
-        }
-        arr[i]= curr; // 미리 저장해둠
-
-        vec.push_back(i);
-        // 폭발 문자열인지 확인
-        if (curr==explode) {
-            for (int j=0; j<explode.length(); j++){
-                vec.pop_back();
-            }
-            
-            // 폭발 이후 새롭게 참조할 curr
-            if (vec.size()!=0) curr = arr[vec.back()];
-            else curr="";
+            if (!stck.empty())
+                idx = stck.top().second;
         }
     }
 
-    if (vec.size()==0) cout << "FRULA";
-    else {
-        for (auto& i: vec){
-            cout << str[i];
-        }   
+    string ans=""; // string을 vector처럼 사용
+    while (!stck.empty()){
+        ans.push_back(stck.top().first);
+        stck.pop();
     }
-    
+
+    reverse(ans.begin(), ans.end());
+    if (ans.length()==0) ans="FRULA";
+    cout << ans;
 
    return 0;
 }
