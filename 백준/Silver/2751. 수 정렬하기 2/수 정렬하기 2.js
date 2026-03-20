07a8f105f3655bc248ebbaf5839c67bfb00335c3
y_mingd <124325672+yminjuu@users.noreply.github.com>
2026-03-20 10:46:03 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1224 ms, Memory: 226908 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.js
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.js
@@ -1,44 +1,10 @@
-function solution(){
-    const input = require("fs").readFileSync(0).toString().trim().split("\n");
-    const N= input[0];
-
-    let arr= [];
-    let tmp=[];
-    for (let i=1; i<=N; i++){
-        const n= Number(input[i]);
-        arr.push(n);
-    }
-
-    const sort = (start, end) => {
-        if (start >= end) return;
-
-        const mid = Math.floor((start+end)/2);
-        sort(start, mid);
-        sort(mid+1, end);
-        merge(start, mid, end);
-    }
-
-    const merge = (start, mid, end) => {
-        let p = start;
-        let q = mid+1;
-        let idx=  start;
-
-        while (p<=mid && q<=end) {
-            if (arr[p]> arr[q]) tmp[idx++]=arr[q++];
-            else tmp[idx++]= arr[p++];
-        }
-
-        while (p<=mid) tmp[idx++]= arr[p++];
-        while (q<=end) tmp[idx++]= arr[q++];
-
-        for (let i=start; i<=end; i++){
-            arr[i]= tmp[i];
-        }
-    }
-
-    let s=0; let e=arr.length-1;
-    sort(s, e);
-    console.log(arr.join("\n"));
-}
-
-solution();
+const input = require("fs")
+    .readFileSync("/dev/stdin")
+    .toString()
+    .trim()
+    .split("\n");
+const N= input[0];
+let arr= [];
+for (let i=1; i<=N; i++) arr.push(Number(input[i]));
+arr.sort((a,b)=> a-b);
+console.log(arr.join("\n"));
