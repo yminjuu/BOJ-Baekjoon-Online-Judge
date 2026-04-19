553c962dff910e48a78f8fc96c54f193cf36c81f
y_mingd <124325672+yminjuu@users.noreply.github.com>
2026-04-19 17:57:57 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.25 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -1,56 +1,45 @@
 #include <vector>
 #include <queue>
-#include <stack>
 #include <iostream>
 #include <climits>
 
 using namespace std;
-using p = pair<int,int>;
-
-vector<vector<int> > g_map;
-int N,M;
-int visited[102][102];
-int ans[102][102]; // 이 좌표까지 가는 최솟값
+vector<vector<int> > gmaps;
 int dx[4]= {0,0,1,-1};
 int dy[4]= {1,-1,0,0};
+int N,M;
 
-void bfs(int x, int y){
+using p = pair<int,int>;
+
+int bfs(){
     queue<p> q;
-    q.push({x,y});
-    visited[x][y]= 1;
-    ans[x][y]= 1;
+    q.push({0,0});
+    int path[102][102];
+    fill(&path[0][0], &path[101][101]+1, 0);
     
-    while (!q.empty()) {
-        x= q.front().first;
-        y= q.front().second;
-        q.pop();
+    path[0][0]= 1;
+    while (!q.empty()){
+        auto [x,y]= q.front(); q.pop();
         
         for (int d=0; d<4; d++){
-            int nextX= x+dx[d];
-            int nextY= y+dy[d];
-            if (nextX>=0 && nextY>=0 && nextX<N && nextY<M && !visited[nextX][nextY] && g_map[nextX][nextY]!=0){
-                visited[nextX][nextY]= 1;
-                q.push({nextX,nextY});
-                ans[nextX][nextY]= ans[x][y]+1; // 이때만 최초 발견/ 최단 거리이므로 넣어줌
+            int newX = x+dx[d]; int newY= y+dy[d];
+            if (newX>=0 && newY>=0 && newX<N && newY<M && gmaps[newX][newY] && !path[newX][newY]) {
+                path[newX][newY]= path[x][y]+1;
+                q.push({newX, newY});
             }
         }
     }
+    
+    return path[N-1][M-1];
 }
 
 // 0:벽, 1: 벽 없음
 int solution(vector<vector<int> > maps)
 {
-    g_map= maps;
-   N= maps.size(); M =maps[0].size();
+    gmaps= maps;
+    N = maps.size(); M = maps[0].size();
     
-    for (int i=0; i<N; i++){
-        for (int j=0; j<M; j++){
-            cin >> maps[i][j];
-        }
-    }
-    
-    bfs(0,0);
-    
-    if (ans[N-1][M-1]==0) return -1;
-    else return ans[N-1][M-1];
+    int ans = bfs();
+    if (ans==0) ans= -1;
+    return ans;
 }
