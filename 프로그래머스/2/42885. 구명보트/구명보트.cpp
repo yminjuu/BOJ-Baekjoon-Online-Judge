3894a3ef242a97596fb4a7edb04e7d2ec41698d4
y_mingd <124325672+yminjuu@users.noreply.github.com>
2026-04-20 10:12:33 +0900

    [level 2] Title: 구명보트, Time: 1.76 ms, Memory: 4.62 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.cpp
+++ 프로그래머스/2/42885. 구명보트/구명보트.cpp
@@ -2,31 +2,22 @@
 #include <vector>
 #include <queue>
 #include <algorithm>
+#include <iostream>
 
 using namespace std;
 
-// 최대한 적은 개수의 보트를 사용해서 사람들을 태워야 한다
-// 최대한 limit에 꽉 차게
 int solution(vector<int> people, int limit) {
     int answer = 0; 
     
-    sort(people.begin(), people.end()); 
-    // 정렬 내림차순은?
-    deque<long> deq(people.begin(), people.end());
-        
-    while (deq.size()>=2){
-        int last = deq.back();
-        deq.pop_back();
-        
-        if (deq.size() >=1 && last + deq.front()<=limit) {
-            last += deq.front();
-            deq.pop_front();
-        }
-        
-        answer++;
-    }
+    // 정렬 후 투포인터 사용
+    sort(people.begin(), people.end());
     
-    if (deq.size()==1) {
+    int s = 0; int e = people.size() - 1;
+    while (s<=e){
+        if (people[s] + people[e] <= limit){
+            s++;
+            e--;
+        } else e--;
         answer++;
     }
     
