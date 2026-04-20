bed66aed1f46268d46737cd64155ee2ced023cf3
y_mingd <124325672+yminjuu@users.noreply.github.com>
2026-04-20 10:51:18 +0900

    [level 2] Title: 큰 수 만들기, Time: 1209.72 ms, Memory: 5.53 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/README.md
+++ 프로그래머스/2/42883. 큰 수 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 큰 수 만들기 - 42883 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42883#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42883) 
 
 ### 성능 요약
 
-메모리: 5.55 MB, 시간: 5777.26 ms
+메모리: 5.53 MB, 시간: 1209.72 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 30일 12:17:48
+2026년 04월 20일 10:51:16
 
 ### 문제 설명
 
--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
@@ -2,31 +2,37 @@
 #include <vector>
 #include <queue>
 #include <algorithm>
+#include <stack>
+#include <iostream>
 
 using namespace std;
 
 string solution(string number, int k) {
     string answer= "";
     
-    int biggest = 0;
-    int idx = 0;
-    // k에 하나씩 더해서 k와 number size이 같아질 때까지
-    for (int i=0; i<number.size(); i++) {
-        if (number[i] > biggest) {
-            biggest = number[i];
-            idx= i; // 가장 큰 수의 인덱스를 기억한다.
+    int N= number.length();
+    stack<int> stck;
+    
+    int i=0;
+    for (; i<N; i++){
+        int n = number[i]-'0';
+        
+        // 지금 들어가는 수보다 작은 게 있으면 다 뺀다
+        while ((!stck.empty() && stck.top() < n) && k!=0){
+            stck.pop();
+            k--;
         }
-        
-        
-        // 최댓값을 갱신해주다가 k(멈춰서서 해당 최댓값을 answer에 넣어줘야 하는 위치)
-        if (i==k) {
-            answer.push_back(number[idx]);
-            k++;
-            i= idx; // 현재 인덱스 다음 위치부터 찾도록
-            biggest =0; idx=0;
-        }
-        
+        stck.push(n);
     }
+    
+    // 내림차순으로 완성된 상황
+    while (!stck.empty()){
+        int n = stck.top(); stck.pop();
+        answer = to_string(n) + answer;
+    }
+    
+    // 자르기
+    answer = answer.substr(0, N-k);
     
     return answer;
 }
