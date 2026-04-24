9390d37d4e610ef54a1d09ba64a2787c03166599
y_mingd <124325672+yminjuu@users.noreply.github.com>
2026-04-24 11:25:47 +0900

    [level 2] Title: 프로세스, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.cpp
+++ 프로그래머스/2/42587. 프로세스/프로세스.cpp
@@ -3,42 +3,33 @@
 #include <queue>
 
 using namespace std;
-
-// 우선순위가 높다 = 숫자가 크다
-// 인덱스마다 priority를 갖는다.
-    // priority가 큰 순서 => 앞에 있는 순서
-
-// struct cmp{
-//     bool operator()(pair<int,int> &a, pair<int,int> &b){
-//         if (a.second==b.second) return a.first > b.first;
-//         else return a.second < b.second;
-//     }
-// };
+using p= pair<int,int>;
 
 int solution(vector<int> priorities, int location) {
-    priority_queue<int, vector<int>> pq; // 가장 큰 원소가 맨 위, pq
-    deque<int> dq;
+    priority_queue<int, vector<int>> pq; // 우선순위 존재 확인용
+    queue<p> q;
     
     for (int i=0; i<priorities.size(); i++){
         pq.push(priorities[i]);
-        dq.push_back(i); // 인덱스를 넣음 (순서 자체)
+        q.push({priorities[i], i});
     }
     
-    int answer=1; int idx=0;
-    while (true){
-        if (priorities[dq.front()]!=pq.top()){
-            int tmp= dq.front();
-            dq.pop_front();
-            dq.push_back(tmp);
-            // 건너뛰기
+    int idx=1; int ans;
+    while (!pq.empty() || !q.empty()){
+        if (pq.top() > q.front().first) {
+            // 아직 못 넣음
+            p process = q.front();
+            q.pop();
+            q.push(process);
         } else {
-            if (dq.front()==location) return answer;
-            else {
-                // 실행
-                dq.pop_front();
-                pq.pop();
-                answer++;
+            if (q.front().second==location) {
+                ans = idx;
+                break;
             }
+            pq.pop();
+            q.pop();
+            idx++;
         }
     }
+    return ans;
 }
