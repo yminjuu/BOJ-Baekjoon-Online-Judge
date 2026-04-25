20f216e0251d8709c717a53b35b33931188bad0e
y_mingd <124325672+yminjuu@users.noreply.github.com>
2026-04-25 10:43:29 +0900

    [level 4] Title: 저자 별 카테고리 별 매출액 집계하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/저자 별 카테고리 별 매출액 집계하기.sql
+++ 프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/저자 별 카테고리 별 매출액 집계하기.sql
@@ -1,9 +1,15 @@
--- 코드를 입력하세요
-# 2022년 1월 기준 책 별 판매량 + 책 별 author id
-select AUTHOR_ID, AUTHOR_NAME, CATEGORY, sum(a.sales*b.price) as TOTAL_SALES
-from
-(SELECT BOOK_ID, SALES
-FROM BOOK_SALES
-WHERE SALES_DATE LIKE ('2022-01%')) a natural join BOOK b natural join AUTHOR c
-group by c.author_name, category
-order by 1, 3 desc;
+select author_id, author_name, category, sum(sales*price) as total_sales
+from book_sales join book using (book_id) join author using (author_id)
+where sales_date like ('2022-01%')
+group by author_id, category
+order by author_id, category desc;
+
+# -- 코드를 입력하세요
+# # 2022년 1월 기준 책 별 판매량 + 책 별 author id
+# select AUTHOR_ID, AUTHOR_NAME, CATEGORY, sum(a.sales*b.price) as TOTAL_SALES
+# from
+# (SELECT BOOK_ID, SALES
+# FROM BOOK_SALES
+# WHERE SALES_DATE LIKE ('2022-01%')) a natural join BOOK b natural join AUTHOR c
+# group by c.author_name, category
+# order by 1, 3 desc;
